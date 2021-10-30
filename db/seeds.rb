e6529040c345cbbe0e2a8536fec703955e9e84b3
Elton Santos <eltonaxl@hotmail.com>
2021-10-30 04:58:04 -0300

    Tentando resolver problema de permissao

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: ENV["ADMIN_EMAIL"], password: ENV["ADMIN_PASSWORD"], password_confirmation: ENV["ADMIN_PASSWORD"])
+User.create(email: ENV["ADMIN_EMAIL"], password: ENV["ADMIN_PASSWORD"], password_confirmation: ENV["ADMIN_PASSWORD"], role: 1)
+
+Category.create(name: "Dor de cabeça")
+Category.create(name: "Dor de barriga")
+Category.create(name: "Febre")
