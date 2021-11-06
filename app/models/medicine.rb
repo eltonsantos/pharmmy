b9a091e1366c643eecc05084853384b1312c901f
Elton Santos <eltonaxl@hotmail.com>
2021-11-06 20:55:21 -0300

    Colocando um dependent destroy no model medicine

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -1,5 +1,5 @@
 class Medicine < ApplicationRecord
-  has_many :medicine_categories
+  has_many :medicine_categories, dependent: :destroy
   has_many :categories, through: :medicine_categories
 
   has_one_attached :picture
