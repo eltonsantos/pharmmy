19a6f39cac09d27bd9cb3083fd207598d41a2260
Elton Santos <eltonaxl@hotmail.com>
2021-12-12 05:37:06 -0300

    Adicionando toastr nas notificacoes e alertas

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "../stylesheets/application";
 import "@fortawesome/fontawesome-free/js/all";
 import "@fortawesome/fontawesome-free/css/all";
 
+global.toastr = require("toastr")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
