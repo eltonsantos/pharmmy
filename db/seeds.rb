00f6380c3487a12268b5e396458ce5d9a3510b3c
Elton Santos <eltonaxl@hotmail.com>
2021-12-12 15:35:16 -0300

    Adicionando nome na tabela user e modificando o sistema para mostrar o nome ao inves do email

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(email: ENV["ADMIN_EMAIL"], password: ENV["ADMIN_PASSWORD"], password_confirmation: ENV["ADMIN_PASSWORD"], role: ENV["ADMIN_ROLE"])
-User.create(email: "elton@elton.com", password: 123456, password_confirmation: 123456, role: 0)
-User.create(email: "teste@teste.com", password: 123456, password_confirmation: 123456, role: 0)
-User.create(email: "admin@admin.com", password: 123456, password_confirmation: 123456, role: 1)
+User.create(name: "Elton Santos", email: "elton@elton.com", password: 123456, password_confirmation: 123456, role: 0)
+User.create(name: "Ericson Santos", email: "eric@eric.com", password: 123456, password_confirmation: 123456, role: 0)
+User.create(name: "Administrador", email: "admin@admin.com", password: 123456, password_confirmation: 123456, role: 1)
 
 Category.create(name: "Dor de cabeça", user_id: 1)
 Category.create(name: "Dor de barriga", user_id: 1)
