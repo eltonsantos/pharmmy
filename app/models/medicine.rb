d33de7677c90413ad6dba0c0aac9e630fabef639
Elton Santos <eltonaxl@hotmail.com>
2021-12-12 00:22:18 -0300

    Adicionando usuario ao produto

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -2,5 +2,7 @@ class Medicine < ApplicationRecord
   has_many :medicine_categories, dependent: :destroy
   has_many :categories, through: :medicine_categories
 
+  belongs_to :user
+
   has_one_attached :picture
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_30_053029) do
+ActiveRecord::Schema.define(version: 2021_12_12_031406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 2021_10_30_053029) do
     t.date "purchase_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_medicines_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -86,4 +88,5 @@ ActiveRecord::Schema.define(version: 2021_10_30_053029) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "medicine_categories", "categories"
   add_foreign_key "medicine_categories", "medicines"
+  add_foreign_key "medicines", "users"
 end
