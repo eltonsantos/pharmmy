8a3c50644e2ff7d5f06fcae3cc2295a16b19afe4
Elton Santos <eltonaxl@hotmail.com>
2021-12-13 00:02:08 -0300

    Melhorando a traducao

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  # config.i18n.enforce_available_locales = false
-  # config.i18n.available_locales = ["pt-BR"]
-  # config.i18n.default_locale = :'pt-BR'
+  config.i18n.enforce_available_locales = false
+  config.i18n.available_locales = ["pt-BR"]
+  config.i18n.default_locale = :'pt-BR'
   
   # Settings specified here will take precedence over those in config/application.rb.
 
