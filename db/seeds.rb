56d6a8a3122bce9e9da02c95672ad52b5e9f3e9b
Elton Santos <eltonaxl@hotmail.com>
2021-12-13 01:16:52 -0300

    Comentando seeds desnecessarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(email: ENV["ADMIN_EMAIL"], password: ENV["ADMIN_PASSWORD"], password_confirmation: ENV["ADMIN_PASSWORD"], role: ENV["ADMIN_ROLE"])
-User.create(name: "Elton Santos", email: "elton@elton.com", password: 123456, password_confirmation: 123456, role: 0)
-User.create(name: "Ericson Santos", email: "eric@eric.com", password: 123456, password_confirmation: 123456, role: 0)
-User.create(name: "Administrador", email: "admin@admin.com", password: 123456, password_confirmation: 123456, role: 1)
 
-Category.create(name: "Dor de cabeça", user_id: 1)
-Category.create(name: "Dor de barriga", user_id: 1)
-Category.create(name: "Febre", user_id: 1)
+# User.create(name: "Elton Santos", email: "elton@elton.com", password: 123456, password_confirmation: 123456, role: 0)
+# User.create(name: "Ericson Santos", email: "eric@eric.com", password: 123456, password_confirmation: 123456, role: 0)
+# User.create(name: "Administrador", email: "admin@admin.com", password: 123456, password_confirmation: 123456, role: 1)
+
+# Category.create(name: "Dor de cabeça", user_id: 1)
+# Category.create(name: "Dor de barriga", user_id: 1)
+# Category.create(name: "Febre", user_id: 1)
